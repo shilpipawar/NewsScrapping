e83ba90f6d08bb25c0ef961a7f54458544599d06
shilpipawar <shilpipawar@gmail.com>
2019-02-04 20:36:16 -0600

    PORT process.env.PORT

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
-var PORT = 3030;
+var PORT = process.env.PORT | 3030;
 // Initialize Express
 var app = express();
 
