cfa0875c1860d376c251eff2cfebe10ab0ff1a41
shilpipawar <shilpipawar@gmail.com>
2019-02-06 20:13:57 -0600

    Port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 var db = require("./models");
-var PORT = process.env.PORT | 3030;
+var PORT = process.env.PORT || 3030;
 var app = express();
 // Use morgan logger for logging requests
 app.use(logger("dev"));
